d2ee9fcccb4a5a49a13c4afe0ae2db9ebc27ca30
subhajit952 <maiti.subhajit@gmail.com>
2024-07-24 21:54:31 +0530

    check first api

---
+++ src/main/java/dev/subhajit/bookmyshow/service/TicketService.java
@@ -0,0 +1,10 @@
+package dev.subhajit.bookmyshow.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TicketService {
+    public String greet(){
+        return "HELLOWORLD";
+    }
+}
